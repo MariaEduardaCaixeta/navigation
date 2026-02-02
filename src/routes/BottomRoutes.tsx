c01efde5ea3d6fc4a34085e0ee6066af3c8be856
Maria Eduarda Caixeta <mariaeduarda.caixeta@performait.com>
2026-02-02 13:44:53 +0000

    adding customization to tabBar

--- src/routes/BottomRoutes.tsx
+++ src/routes/BottomRoutes.tsx
@@ -1,6 +1,7 @@
 import { Product } from "@/app/Product";
 import { Home } from "@/app/Home";
 import { BottomTabScreenProps, createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialIcons } from "@expo/vector-icons";
 
 export type BottomTabRoutesList = {
     home: undefined
@@ -13,12 +14,44 @@ const Tab = createBottomTabNavigator<BottomTabRoutesList>();
 
 export function BottomTabRoutes() {
     return (
-         <Tab.Navigator 
+        <Tab.Navigator
             initialRouteName="home"
-            screenOptions={{ headerShown: false }}
+            screenOptions={{ 
+                headerShown: false,
+                tabBarActiveTintColor: '#000', // Exemplo de customização, não usa a cor padrão do sistema
+                tabBarInactiveTintColor: '#888',
+                tabBarLabelPosition: 'beside-icon',
+                
+            }}
         >
-            <Tab.Screen name="home" component={Home} />
-            <Tab.Screen name="product" component={Product} />
-         </Tab.Navigator>
+            <Tab.Screen
+                name="home"
+                component={Home}
+                options={{
+                    tabBarLabel: 'Início',
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialIcons 
+                            name="home"
+                            size={size}
+                            color={color}
+                        />
+                    ),
+                }}
+            />
+            <Tab.Screen 
+                name="product" 
+                component={Product} 
+                options={{
+                    tabBarLabel: 'Produto',
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialIcons 
+                            name="shopping-cart"
+                            size={size}
+                            color={color}
+                        />
+                    ),
+                }}
+            />
+        </Tab.Navigator>
     )
 }
