4a8cfac56aec652c5f1a4c4948f95090d28f924d
Maria Eduarda Caixeta <mariaeduarda.caixeta@performait.com>
2026-02-02 12:24:48 +0000

    adding global navigation type

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -3,13 +3,16 @@ import { View } from "react-native";
 import { Header } from "@/components/Header";
 import { Title } from "@/components/Title";
 import { ButtonIcon } from "@/components/ButtonIcon";
+import { useNavigation } from "@react-navigation/native";
 
 export function Home() {
+    const navigation = useNavigation();
+
     return (
         <View style={{ flex: 1, padding: 32}}>
             <Header>
                 <Title>Home</Title>
-                <ButtonIcon name="add-circle" />
+                <ButtonIcon name="add-circle" onPress={() => navigation.navigate("product")}/>
             </Header>
         </View>
     )
