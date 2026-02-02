be5efa6502d512452d9eb2e3348f9d09dd3fb70c
Maria Eduarda Caixeta <mariaeduarda.caixeta@performait.com>
2026-02-02 13:08:43 +0000

    passing params through route

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -10,7 +10,7 @@ export function Home({ navigation }: StackRoutesProps<'home'>) {
         <View style={{ flex: 1, padding: 32}}>
             <Header>
                 <Title>Home</Title>
-                <ButtonIcon name="add-circle" onPress={() => navigation.navigate("product")}/>
+                <ButtonIcon name="add-circle" onPress={() => navigation.navigate("product", { id: "123" })}/>
             </Header>
         </View>
     )
