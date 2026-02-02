6acb99973f5b24e47259b657395a7488cb1d6f63
Maria Eduarda Caixeta <mariaeduarda.caixeta@performait.com>
2026-02-02 12:06:57 +0000

    adding native navigation

---
+++ src/app/Product.tsx
@@ -0,0 +1,16 @@
+import { View } from "react-native";
+
+import { Header } from "@/components/Header";
+import { Title } from "@/components/Title";
+import { ButtonIcon } from "@/components/ButtonIcon";
+
+export function Product() {
+    return (
+        <View style={{ flex: 1, padding: 32}}>
+            <Header>
+                <ButtonIcon name="arrow-circle-left" />
+                <Title>Product</Title>
+            </Header>
+        </View>
+    )
+}
---
+++ src/routes/StackRoutes.tsx
@@ -0,0 +1,15 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import { Product } from "@/app/Product";
+import { Home } from "@/app/Home";
+
+const Stack = createNativeStackNavigator();
+
+export function StackRoutes() {
+    return (
+         <Stack.Navigator>
+            <Stack.Screen name="home" component={Home} />
+            <Stack.Screen name="product" component={Product} />
+         </Stack.Navigator>
+    )
+}
